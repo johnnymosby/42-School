24f97f134ecbec8dda488fc56c6c0bf18a8c8a4d
Ruslan Basyrov <rbasyrov@c2r5p7.42vienna.com>
2022-09-06 21:01:46 +0200

    Finished C01 ex08

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -11,70 +11,29 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdio.h>
 
-void	ft_swap(int *a, int *b);
-void	ft_quick_sort(int *tab, int a, int b);
 void	ft_sort_int_tab(int *tab, int size);
-
-void	ft_swap(int *a, int *b)
-{
-	int	point;
-
-	point = *a;
-	*a = *b;
-	*b = point;
-}
-
-void	ft_quick_sort(int *tab, int first, int last)
-{
-	int	center;
-	int	a;
-	int	b;
-
-	if (first < last)
-	{
-		center = last;
-		a = first;
-		b = last;
-		while (a < b)
-		{
-			while(tab[a]<center && a < last)
-			{
-				a++;
-			}
-			while(tab[b]>center)
-			{
-				b--;
-			}
-			if (a < b)
-			{
-				ft_swap(&tab[a], &tab[b - 1]);
-			}
-		}
-	}
-}
 
 void	ft_sort_int_tab(int *tab, int size)
 {
-	ft_quick_sort(tab, 0, size);
-}
+	int	counter1;
+	int	counter2;
+	int	temp;
 
-int main()
-{
-	int arr[] = {100, 32, 20, 43, 15, 56};
-	int size = 6;
-	for (int i=0; i < size; i++)
+	counter1 = 0;
+	while (counter1 < size - 1)
 	{
-		printf("%d", arr[i]);
+		counter2 = counter1;
+		while (counter2 < size)
+		{
+			if (tab[counter1] > tab[counter2])
+			{
+				temp = tab[counter1];
+				tab[counter1] = tab[counter2];
+				tab[counter2] = temp;
+			}
+			counter2++;
+		}
+		counter1++;
 	}
-	int * tab;
-	tab = &arr[0];
-	ft_sort_int_tab(tab, size);
-	for (int i=0; i < size; i++)
-	{
-		printf("%d, ", arr[i]);
-	}
-
-	return 0;
 }
