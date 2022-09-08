0723ad660f95130cd409e0326e1524d2e92e1834
Ruslan Basyrov <rbasyrov@c2r5p5.42vienna.com>
2022-09-08 12:53:57 +0200

    Completed ex01

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbasyrov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 16:59:40 by rbasyrov          #+#    #+#             */
+/*   Updated: 2022/09/06 16:59:40 by rbasyrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	counter;
+
+	counter = 0;
+	while (src[counter])
+	{
+		dest[counter] = src[counter];
+		counter++;
+	}
+	while (counter < n)
+	{
+		dest[counter] = '\0';
+		counter++;
+	}
+	return (dest);
+}
