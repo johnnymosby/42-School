f7ae341dd4d3c9860300144245189a6538a4e7e5
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2022-09-13 01:44:12 +0200

    changes to C03

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -10,30 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	int	n;
-
-	n = 0;
-	while (*(str + n) != '\0')
-	{
-		n++;
-	}
-	return (n);
-}
-
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 
 	i = 0;
-	if (ft_strlen(s1) != ft_strlen(s2))
-		return (ft_strlen(s1) - ft_strlen(s2));
-	while (s1[i])
+	while (s1[i] && s2[i])
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -10,27 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(char *str)
-{
-	unsigned int	n;
-
-	n = 0;
-	while (*(str + n) != '\0')
-	{
-		n++;
-	}
-	return (n);
-}
-
-unsigned int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	if (n >= ft_strlen(s1) && n >= ft_strlen(s2))
-		if (ft_strlen(s1) != ft_strlen(s2))
-			return (ft_strlen(s1) - ft_strlen(s2));
-	while (s1[i] && i < n)
+	while ((s1[i] || s2[i]) && (i < n))
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
