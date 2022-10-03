ba89899fdfbfb760a1b710aaf3e598bceb29d633
Ruslan Basyrov <rbasyrov@c2r6p6.42vienna.com>
2022-10-03 12:57:44 +0200

    The exs from C Piscine have been added to a separate folder, libft has been initiated

---
+++ Core Education/includes/ft_isalpha.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbasyrov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 12:51:41 by rbasyrov          #+#    #+#             */
+/*   Updated: 2022/09/08 13:36:13 by rbasyrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalpha(char *str);
+
+int	ft_isalpha(char *str)
+{
+	int		counter;
+	char	value;
+
+	counter = 0;
+	while (str[counter])
+	{
+		value = str[counter];
+		if (!((value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z')))
+		{
+			return (0);
+		}
+		counter++;
+	}
+	return (1);
+}
