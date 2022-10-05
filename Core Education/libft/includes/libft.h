d87b90aba4d6d10cd91481954eec9b072e478feb
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2022-10-05 23:49:43 +0200

    Initiated other functions from the main part

--- Core Education/libft/includes/libft.h
+++ Core Education/libft/includes/libft.h
@@ -23,30 +23,31 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-
+char 	*ft_itoa(int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *a1, const void *a2, size_t size);
 void	*ft_memcpy(void *to, const void *from, size_t size);
 void	*ft_memmove(void *to, const void *from, size_t size);
 void	*ft_memset(void *block, int c, size_t size);
-
-
-
-
-
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char 	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *string, int c);
 char	*ft_strdup(char *src);
-
-
+void 	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char 	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t size);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *string, int c);
 char	*ft_strrchr(const char *string, int c);
-
-
+char 	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
 
--- Core Education/libft/srcs/ft_substr.c
+++ Core Education/libft/srcs/ft_substr.c
@@ -10,4 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+
+}
