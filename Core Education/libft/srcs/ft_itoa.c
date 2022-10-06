c8737100e90ed8ae5854f965bea362774a6741db
Ruslan Basyrov <rbasyrov@c2r5p5.42vienna.com>
2022-10-06 17:43:54 +0200

    Working on 'itoa'

--- Core Education/libft/srcs/ft_itoa.c
+++ Core Education/libft/srcs/ft_itoa.c
@@ -10,63 +10,86 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
+static char	*ft_strcpy(char *dest, char *src)
 {
 	int	counter;
 
 	counter = 0;
-	while (*(str + counter) != '\0')
+	while (src[counter])
 	{
-		write(1, (str + counter), 1);
+		dest[counter] = src[counter];
 		counter++;
 	}
+	dest[counter] = '\0';
+	return (dest);
 }
 
-void	ft_putnbr(int nb)
+static char	*ft_charcat(char *dest, char src)
 {
-	if (nb == -2147483648)
-	{
-		ft_putstr("-2147483648");
-	}
-	else if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb != -2147483648)
-	{
-		if (nb >= 10)
-		{
-			ft_putnbr(nb / 10);
-			ft_putnbr(nb % 10);
-		}
-		else
-		{
-			ft_putchar(nb + '0');
-		}
-	}
+	unsigned int	i;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	dest[i] = src;
+	dest[i + 1] = '\0';
+	return (dest);
+}
+
+static int	ft_pow(int nb, int power)
+{
+	int	value;
+
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	value = nb;
+	while (--power > 0)
+		nb *= value;
+	return (nb);
 }
 
 char	*ft_itoa(int n)
 {
-	static char	*str;
-	int			len;
+	char	*str;
+	int		i;
+	int		nn;
+	int		sign;
 
 	if (n == -2147483648)
 	{
 		str = malloc(12);
 		if (!str)
 			return (0);
-		ft_itoa(-2);
-		ft_itoa(147483648);
+		str = ft_strcpy(str, "-2147483648");
+		return (str);
 	}
-
+	sign = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		sign = 1;
+	}
+	i = 0;
+	nn = n;
+	while (nn / 10 != 0)
+	{
+		nn /= 10;
+		i++;
+	}
+	str = malloc(i + sign + 1);
+	if (!str)
+		return (0);
+	if (sign == 1)
+		str = ft_strcpy(str, "-");
+	while (i != -1)
+	{
+		str = ft_charcat(str, (n / ft_pow(10, i)) + '0');
+		n %= ft_pow(10, i);
+		i--;
+	}
+	return (str);
 }
-
