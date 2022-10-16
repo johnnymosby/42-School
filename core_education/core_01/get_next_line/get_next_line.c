a4fac7361f5e9ee13cdcb302f98d91e0bd9c2a16
Ruslan Basyrov <rbasyrov@c2r5p5.42vienna.com>
2022-10-16 19:27:01 +0200

    Somewhat working gnl

--- core_education/core_01/get_next_line/get_next_line.c
+++ core_education/core_01/get_next_line/get_next_line.c
@@ -10,3 +10,89 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int	nchar_read(int fd, char *buf)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while (j)
+	{
+		j = read(fd, buf + i, 1);
+		i++;
+	}
+	return (i);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buf;
+	static int	start;
+	static int	nchar;
+	char		*ret;
+	int			j;
+	int 		i;
+
+	if (nchar == 0)
+	{
+		buf = malloc(BUFFER_SIZE);
+		if (!buf)
+		{
+			free(buf);
+			return (0);
+		}
+		nchar = nchar_read(fd, buf);
+	}
+	if (nchar < 1)
+	{
+		free(buf);
+		return (0);
+	}
+	j = 0;
+	while ((start + j) <= nchar && (start + j) <= BUFFER_SIZE)
+	{
+		if (buf[start + j] == '\n')
+		{
+			j++;
+			break ;
+		}
+		j++;
+	}
+	if (!j)
+	{
+		free(buf);
+		return (0);
+	}
+	ret = malloc(j + 1);
+	if (!ret)
+	{
+		free(buf);
+		return (0);
+	}
+	ret[j] = '\0';
+	i = j;
+	while (--j >= 0)
+		ret[j] = buf[start + j];
+	start = start + i;
+/*	if (start == BUFFER_SIZE - 1)
+		free(buf);*/
+	return (ret);
+}
+
+int	main(void)
+{
+	int		fd;
+
+	fd = open("test", O_RDWR);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	close(fd);
+}
--- core_education/core_01/get_next_line/get_next_line.h
+++ core_education/core_01/get_next_line/get_next_line.h
@@ -10,3 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
