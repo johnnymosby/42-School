f3b535b95007e082088c82be58f3c58529304a5f
Ruslan Basyrov <rbasyrov@c1r1p9.42vienna.com>
2022-11-08 15:18:01 +0100

    Started learning minilibx

---
+++ core_education/core_02/fractol/fractol.c
@@ -0,0 +1,51 @@
+#include <mlx.h>
+
+#include <mlx.h>
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *) dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+	int		x;
+	int		y;
+	int		z;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	x = 1920 / 3;
+	while (x < (1920 * 2 / 3))
+	{
+		z = 0;
+		y = 1080 / 3;
+		while (y + z < (1080 * 2 / 3))
+		{
+			my_mlx_pixel_put(&img, x, y, 0x00FF0000 + z);
+			z++;
+			y++;
+		}
+		x++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
+
