bed0fd7ff7e442fa5a920a9dd12684cb302a2a5b
Ruslan Basyrov <rbasyrov@c1r1p9.42vienna.com>
2022-11-08 16:18:34 +0100

    ...

--- core_education/core_02/fractol/fractol.c
+++ core_education/core_02/fractol/fractol.c
@@ -1,4 +1,14 @@
-#include <mlx.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 15:32:01 by rbasyrov          #+#    #+#             */
+/*   Updated: 2022/11/08 16:14:31 by rbasyrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <mlx.h>
 
@@ -18,18 +28,12 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *) dst = color;
 }
 
-int	main(void)
+void	add_picture(t_data img)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
 	int		x;
 	int		y;
 	int		z;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
 	x = 1920 / 3;
@@ -45,7 +49,26 @@ int	main(void)
 		}
 		x++;
 	}
+}
+
+int	close(int keycode, t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	add_picture(img);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_hook(mlx_win, 2, 1L<<0, close, void *param);
 	mlx_loop(mlx);
 }
 
