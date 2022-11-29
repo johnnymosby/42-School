4baabd13043205007e935b331d7826e96621cd6a
Ruslan Basyrov <rbasyrov@c2r4p2.42vienna.com>
2022-11-29 16:41:21 +0100

    Added change of location by moving mouse

--- core_education/core_02/fractol/inc/fractol.h
+++ core_education/core_02/fractol/inc/fractol.h
@@ -6,7 +6,7 @@
 /*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:24:12 by rbasyrov          #+#    #+#             */
-/*   Updated: 2022/11/29 13:44:28 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2022/11/29 16:12:57 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@
 
 typedef struct s_image_fr {
 	void	*img;
+	void	*img2;
 	char	*addr;
+	char	*addr2;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
@@ -33,13 +35,19 @@ typedef struct s_image_fr {
 	int		if_to_render;
 	int		width;
 	int		height;
+	int		x;
+	int		y;
+	int		what_image;
 }	t_image_fr;
 
 void	new_mlx_pixel_put(t_image_fr *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	if (data->what_image == 0)
+		dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	else
+		dst = data->addr2 + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *) dst = color;
 }
 
--- core_education/core_02/fractol/src/fractol.c
+++ core_education/core_02/fractol/src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:12:33 by rbasyrov          #+#    #+#             */
-/*   Updated: 2022/11/29 14:54:54 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2022/11/29 16:16:25 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ void	check_input(int argc, char *argv[])
 int	exit_fractol(t_image_fr *fr)
 {
 	mlx_destroy_image(fr->mlx, fr->img);
+	mlx_destroy_image(fr->mlx, fr->img2);
 	mlx_destroy_window(fr->mlx, fr->mlx_win);
 	exit(EXIT_SUCCESS);
 }
@@ -42,6 +43,7 @@ int	exit_fractol(t_image_fr *fr)
 void	exit_with_message(t_image_fr *fr, char *message)
 {
 	mlx_destroy_image(fr->mlx, fr->img);
+	mlx_destroy_image(fr->mlx, fr->img2);
 	mlx_destroy_window(fr->mlx, fr->mlx_win);
 	ft_putstr_fd(message, STDERR_FILENO);
 	exit(EXIT_FAILURE);
@@ -56,16 +58,22 @@ void	init_t_image_fr(t_image_fr	*fr, char *choice_fr)
 	if (fr->mlx_win == NULL)
 		exit_with_message(fr, "Error: mlx_new_window() failed");
 	fr->img = mlx_new_image(fr->mlx, FR_WIDTH, FR_HEIGHT);
-	if (fr->img == NULL)
+	fr->img2 = mlx_new_image(fr->mlx, FR_WIDTH, FR_HEIGHT);
+	if (fr->img == NULL || fr->img2 == NULL)
 		exit_with_message(fr, "Error: mlx_new_image() failed");
 	fr->addr = mlx_get_data_addr(fr->img, &fr->bits_per_pixel,
 			&fr->line_length, &fr->endian);
-	if (fr->addr == NULL)
+	fr->addr2 = mlx_get_data_addr(fr->img2, &fr->bits_per_pixel,
+			&fr->line_length, &fr->endian);
+	if (fr->addr == NULL || fr->addr2 == NULL)
 		exit_with_message(fr, "Error: mlx_get_data_addr() failed");
 	fr->usr_choice = choice_fr;
 	fr->if_to_render = 0;
 	fr->width = FR_WIDTH;
 	fr->height = FR_HEIGHT;
+	fr->x = 0;
+	fr->y = 0;
+	fr->what_image = 0;
 }
 
 void	draw_mandelbrot(t_image_fr *fr)
@@ -79,7 +87,8 @@ void	draw_mandelbrot(t_image_fr *fr)
 		j = 0;
 		while (j < fr->height)
 		{
-			if ((i % 100 < 50 && j % 100 < 50) || (i % 100 >= 50 && j % 100 >= 50))
+			if (((i + fr->x) % 100 < 50 && (j + fr->y) % 100 < 50)
+				|| ((i + fr->x) % 100 >= 50 && (j + fr->y) % 100 >= 50))
 				new_mlx_pixel_put(fr, i, j, 0x00FAFA6E);
 			else
 				new_mlx_pixel_put(fr, i, j, 0x00089F8F);
@@ -95,16 +104,34 @@ int	render_image(t_image_fr *fr)
 	{
 		if (ft_strcmp(fr->usr_choice, "mandelbrot") == 0)
 			draw_mandelbrot(fr);
-		mlx_put_image_to_window(fr->mlx, fr->mlx_win, fr->img, 0, 0);
+		if (fr->what_image == 0)
+			mlx_put_image_to_window(fr->mlx, fr->mlx_win, fr->img, 0, 0);
+		else
+			mlx_put_image_to_window(fr->mlx, fr->mlx_win, fr->img2, 0, 0);
+		fr->what_image = (fr->what_image + 1) % 2;
 		fr->if_to_render = 1;
 	}
 	return (fr->if_to_render);
 }
 
-int	pressed_key(int keycode, t_image_fr *fr)
+int	pressed_key(int button, t_image_fr *fr)
 {
-	if (keycode == 27)
+	if (button == 53 || button == 65307)
 		exit_fractol(fr);
+	return (0);
+}
+
+
+
+int mouse_hook(int button, int x, int y, t_image_fr *fr)
+{
+	if (button == 1)
+	{
+		fr->x = x;
+		fr->y = y;
+		fr->if_to_render = 0;
+		render_image(fr);
+	}
 	return (0);
 }
 
@@ -116,6 +143,7 @@ int	main(int argc, char *argv[])
 	init_t_image_fr(&fr, argv[1]);
 	mlx_loop_hook(fr.mlx, render_image, &fr);
 	mlx_key_hook(fr.mlx_win, pressed_key, &fr);
+	mlx_mouse_hook(fr.mlx_win, mouse_hook, &fr);
 	mlx_hook(fr.mlx_win, 17, 0L, exit_fractol, &fr);
 	mlx_loop(fr.mlx);
 }
