9b317431c5aa20698c1010bb27fee0f22ead1e26
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2022-11-29 10:52:49 +0100

    Trying to make the program start respecting the possible errors

--- core_education/core_02/fractol/inc/fractol.h
+++ core_education/core_02/fractol/inc/fractol.h
@@ -1,24 +1,42 @@
-#include "../lib/mlx_osx/mlx.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbasyrov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 16:24:12 by rbasyrov          #+#    #+#             */
+/*   Updated: 2022/11/29 01:36:13 by rbasyrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define NITERATION = 250
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# include "../lib/mlx_osx/mlx.h"
+# include "../lib/mlx_osx/mlx.h"
+# include "../lib/libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# define MAX_N_ITERATION = 250
+# define FR_WIDTH = 1280
+# define FR_HEIGHT = 720
 
-typedef struct {
+typedef struct s_image_fr {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-	int		xlen;
-	int		ylen;
-	int		sf;		//scaling factor
 	void	*mlx;
-	void	*win;
-}				img_mlx;
+	char	*usr_choice;
+}	t_image_fr;
 
-void	new_mlx_pixel_put(img_mlx *data, int x, int y, int color)
+void	new_mlx_pixel_put(t_image_fr *data, int x, int y, int color)
 {
-	char *dst;
+	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *) dst = color;
 }
+
+#endif
--- core_education/core_02/fractol/src/fractol.c
+++ core_education/core_02/fractol/src/fractol.c
@@ -6,25 +6,72 @@
 /*   By: rbasyrov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:12:33 by rbasyrov          #+#    #+#             */
-/*   Updated: 2022/11/24 13:15:52 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2022/11/29 10:51:22 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
 
-int	main(void)
+int	check_input(int argc, char *argv[])
 {
-	img_mlx	img;
+	if (argc > 2)
+	{
+		ft_putstr_fd("Too many arguments", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	else if (argc < 2)
+	{
+		ft_putstr_fd("Too few arguments [mandelbrot, julia]", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	else if (ft_strcmp(argv[1], "mandelbrot") != 0
+		&& ft_strcmp(argv[1], "julia") != 0)
+	{
+		ft_putstr_fd("Wrong argument [mandelbrot, julia]", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+}
 
-	img.xlen = 1280;
-	img.ylen = 720;
-	img.mlx = mlx_init();
-	img.win = mlx_new_window(img.mlx, img.xlen, img.ylen, "Hello world!");
-	img.img = mlx_new_image(img.mlx, img.xlen, img.ylen);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								 &img.endian);
-	new_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(img.mlx, img.win, img.img, 0, 0);
-	mlx_hook(img.win,  17, 0L, close, &img);
-	mlx_loop(img.mlx);
+void	init_t_image_fr(t_image_fr	*fr, char *choice_fr)
+{
+	fr->mlx = mlx_init();
+	if (fr->mlx == NULL)
+	{
+		ft_putstr_fd("A problem with mlx intialisation", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	fr->img = mlx_new_image(fr->mlx, FR_WIDTH, FR_HEIGHT);
+	if (fr->img == NULL)
+	{
+		ft_putstr_fd("A problem with mlx_new_image", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	fr->usr_choice = choice_fr;
+}
+
+void	render_image(t_image_fr *fr)
+{
+	if (ft_strcmp(fr->usr_choice, "mandelbrot") == 0)
+		draw_mandelbrot(fr);
+}
+
+void	draw_mandelbrot(t_image_fr *fr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+}
+
+int	main(int argc, char *argv[])
+{
+	t_image_fr	fr;
+
+	check_input(argc, argv);
+	init_t_image_fr(&fr, argv[1]);
+
+
+	mlx_loop_hook(fr.mlx, render_image, &fr);
+	mlx_loop(fr.mlx);
 }
