c8a15aa178a4d906af4caf083c90c3f385964400
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2023-02-27 14:48:59 +0100

    +cleaning input from extra spaces and zeroes

--- core_education/core_02/push_swap/inc/push_swap.h
+++ core_education/core_02/push_swap/inc/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rbasyrov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 20:46:04 by rbasyrov          #+#    #+#             */
-/*   Updated: 2023/01/22 14:47:10 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2023/02/27 14:07:20 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,27 @@
 # include "stdlib.h"
 # include "unistd.h"
 
+/* typedef struct s_stack_elem
+{
+	t_stack_elem	*next;
+	t_stack_elem	*previous;
+}	t_stack_elem;
+
+typedef struct s_stack
+{
+	t_stack_elem	*top;
+}	t_stack;
+
+t_stack	*transform_input_to_array(char *input, t_stack stack);
+void	initialise_stack(t_stack *stack, char *input); */
+
+void	exit_if_malloc_error(char *s);
+
+
 void	add_space_after_word(char *s);
+void	clean_input(char *input);
 char	*collapse_arguments(int argc, char **argv);
+
 void	exit_if_not_numbers(char *s);
 
 #endif
--- core_education/core_02/push_swap/src/main.c
+++ core_education/core_02/push_swap/src/main.c
@@ -3,22 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rbasyrov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 20:39:47 by rbasyrov          #+#    #+#             */
-/*   Updated: 2023/01/22 14:42:37 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2023/02/27 14:41:00 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	exit_if_malloc_error(char *s)
+{
+	free(s);
+	ft_printf("Wrong input");
+	exit(EXIT_FAILURE);
+}
+
+/* void initialise_stack(t_stack	*stack, char *input)
+{
+	t_stack	*stack;
+
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		exit_if_malloc_error(input);
+	stack->top->previous = NULL;
+	stack->top->next = NULL;
+}
+
+
+t_stack	*transform_input_to_array(char *input, t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	while (input[i] != '\0')
+	
+	
+}
+ */
+
+void	clean_input(char *input)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (input[i] != '\0')
+	{
+		if (i == 0)
+			while (input[i] == ' ')
+				i++;
+		while (input[i] == '0' && input[i + 1] != ' ')
+			i++;
+ 		while (input[i] == ' ' && input[i + 1] == ' ')
+			i++;
+		input[j] = input[i];
+		i++;
+		j++;
+	}
+	input[j] = '\0';
+}
+
+
+
 int	main(int argc, char **argv)
 {
 	char	*input;
+/* 	int		*array;
+	t_stack	*stack_a;
+	t_stack	*stack_b; */
 
 	if (argc < 2)
 		return (1);
 	input = collapse_arguments(argc, argv);
 	exit_if_not_numbers(input);
-	ft_printf("%s", input);
+	clean_input(input);
+/* 	initialise_array(array, input);
+	transform_input_to_array(array, input);
+	initialise_stack(stack_a, input);
+	initialise_stack(stack_b, input);
+	transform_array_to_stack_a(input, stack_a); */
+	ft_printf("%s\n", input);
 }
