05f2a24e3f8dd8adf0f10f6eddaeba0bbee71871
Ruslan Basyrov <rbasyrov@c1r1p8.42vienna.com>
2023-03-28 13:29:23 +0200

    push_swap: small changes

--- core_education/core_02/push_swap/bonus/checker.c
+++ core_education/core_02/push_swap/bonus/checker.c
@@ -6,12 +6,40 @@
 /*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:45:35 by rbasyrov          #+#    #+#             */
-/*   Updated: 2023/03/27 19:58:52 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2023/03/28 11:06:27 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "get_next_line.h"
+
+static int	if_command(char *command)
+{
+	if (ft_strcmp(command, "sa\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "sb\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "ss\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "pa\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "pb\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "ra\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "rb\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "rr\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "rra\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "rrb\n") == 0)
+		return (1);
+	else if (ft_strcmp(command, "rrr\n") == 0)
+		return (1);
+	else
+		return (0);
+}
 
 static void	move(char *command, t_context *ct)
 {
@@ -70,6 +98,8 @@ int	main(int argc, char **argv)
 		cmd = get_next_line(STDIN_FILENO);
 		if (cmd == NULL)
 			break ;
+		if (if_command(cmd) == 0)
+			exit_with_message(ct, "not a command");
 		move(cmd, ct);
 	}
 	if (check_if_sorted(ct) == 1 && ct->st_b->n == 0)
