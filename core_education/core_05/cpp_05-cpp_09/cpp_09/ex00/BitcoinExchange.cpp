6ade6aa4a950c0523c7abb71ad877d708d3a5a77
Ruslan Basyrov <rbasyrov@student.42.fr>
2023-11-07 20:17:49 +0100

    CPP: continued working on cpp09 ex00

--- core_education/core_05/cpp_05-cpp_09/cpp_09/ex00/BitcoinExchange.cpp
+++ core_education/core_05/cpp_05-cpp_09/cpp_09/ex00/BitcoinExchange.cpp
@@ -1,8 +1,98 @@
-#include "BitcointExchange.hpp"
+#include "BitcoinExchange.hpp"
 
-BitcointExchange::BitcointExchange() {}
-BitcointExchange::~BitcointExchange() {}
+BitcoinExchange::BitcoinExchange(): isInitialised(false) {}
+BitcoinExchange::~BitcoinExchange() {}
 
-void BitcointExchange::exchange(std::string entry) {
-	
+void BitcoinExchange::initialise(std::string pathname) {
+	std::ifstream file;
+	std::string line;
+
+	file.open(pathname);
+	if (!file.is_open()) {
+		std::cout << "Error: Failed to open the file\n";
+		return ;
+	}
+	isInitialised = true;
+	getline(file, line);
+	if (line != "date,exchange_rate") {
+		std::cout << "Error: wrong columns\n";
+		file.close();
+		return ;
+	}
+	while (getline(file, line)) {
+		if (!file.eof()) {
+			if (!isValidLine(line))
+				continue ;
+		}
+	}
+}
+
+void BitcoinExchange::exchange(std::string data) {
+	pentry entry;
+
+	if (isInitialised == false) {
+		std::cout << "Error: BitcoinExchange was not initialised\n";
+		return ;
+	}
+
+	if (parseEntry(data, entry) == false) {
+		std::cerr << "Error!\n";
+		return ;
+	}
+}
+
+bool BitcoinExchange::parseEntry(std::string data, pentry &entry) {
+	std::istringstream estream(data);
+
+	estream >> entry.date >> entry.pipe >> entry.value;
+	if (estream.fail() || estream.eof()
+		|| (entry.value < 0 || entry.value > 1000)
+		|| (fabs(entry.value) < EPSILON || fabs(entry.value - 1000) < EPSILON))
+		return false;
+	if (isValidDate(entry.date) == false)
+		return false;
+}
+
+bool BitcoinExchange::isValidDate(std::string date) {
+	if (date.size() != 10 || date[4] != '-' | date[7] != '-')
+		return false;
+
+	std::istringstream estream(date);
+	int year;
+	int month;
+	int day;
+	char sep[2];
+
+	estream >> year >> sep[0] >> month >> sep[1] >> day;
+	if (estream.fail() || estream.eof()
+		|| sep[0] != '-' || sep[1] != '-'
+		|| year < 1
+		|| (month < 1 || month > 12)
+		|| (day < 1 || day > 31)
+		|| (month == 2 && day == 30))
+		return false;
+	if (month >= 8 && month % 2 == 1 && day == 31
+		|| month <= 7 && month % 2 == 0 && day == 31)
+		return false;
+	if (month == 2
+		&& (year % 400 != 0 && year % 100 == 0 || year % 4 != 0)
+		&& day == 29)
+		return false;
+	return true;
+}
+
+bool BitcoinExchange::isValidLine(std::string &line) {
+	bool comma_was_met = 0;
+	bool displayable_char_was_met = false;
+
+	for (std::string::iterator iter = line.begin(); iter != line.end(); iter++) {
+		if (!std::isspace(*iter))
+			displayable_char_was_met = true;
+		if (*iter == ',') {
+			if (comma_was_met != false)
+				return false;
+			comma_was_met = true;
+			
+		}
+	}
 }
--- core_education/core_05/cpp_05-cpp_09/cpp_09/ex00/BitcoinExchange.hpp
+++ core_education/core_05/cpp_05-cpp_09/cpp_09/ex00/BitcoinExchange.hpp
@@ -1,19 +1,40 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
-# include <set>
+# include <map>
+# include <string>
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include <cmath>
+# include <cctype>
+
+const double EPSILON = 1e-9;
+const std::string RED = "\033[31m";
+const std::string RESET = "\033[0m";
+
+typedef struct
+{
+	std::string	date;
+	char		pipe;
+	double		value;
+}	pentry;
 
 class BitcoinExchange
 {
 	public:
 		BitcoinExchange();
 		~BitcoinExchange();
-		void exchange(std::string entry);
+		void exchange(std::string data);
+		void initialise(std::string pathname);
 
 	private:
-		bool parseEntry(std::string entry);
+		bool parseEntry(std::string data, pentry &entry);
+		bool isValidDate(std::string date);
+		bool isValidLine(std::string &line);
 
-		std::multiset<std::string, double> database;
+		std::map<std::string, double> database;
+		bool isInitialised;
 };
 
 #endif
