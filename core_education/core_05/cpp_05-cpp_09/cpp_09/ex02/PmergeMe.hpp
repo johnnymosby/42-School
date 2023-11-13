af021e53a9932fca7c9c80cc90ed336f40b5ce2e
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2023-11-14 00:34:22 +0100

    CPP: cpp09 ex02 changed functions to template functions

--- core_education/core_05/cpp_05-cpp_09/cpp_09/ex02/PmergeMe.hpp
+++ core_education/core_05/cpp_05-cpp_09/cpp_09/ex02/PmergeMe.hpp
@@ -13,7 +13,79 @@
 # include <ctime>
 # include <iomanip>
 
-void sort(std::vector<int> &vec);
-void sort(std::deque<int> &vec);
+const int INDEX_BIGGEST_TYPE_INT_JACOBSTHAL_NUMBER = 33;
+
+const std::vector<int>& getJacobsthalNumbers();
+
+template <class T>
+int binary_search(T const &vec, int target) {
+	int low = 0;
+	int high = vec.size() - 1;
+
+	while (low <= high) {
+		int mid = low + (high - low) / 2;
+
+		if (vec[mid] == target) {
+			return mid;
+		} else if (vec[mid] < target) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+	return low;
+}
+
+template <class T>
+void sort(T &vec_orig) {
+	if (vec_orig.size() <= 1)
+		return ;
+
+	typename T::iterator begin(vec_orig.begin()), end(vec_orig.end());
+	typename T::iterator middle = vec_orig.size() / 2 + begin;
+	T left(begin, middle), right;
+	int leftover = 0;
+	if (vec_orig.size() % 2 != 0) {
+		end--;
+		leftover = *(end);
+	}
+	right = T(middle, end);
+	for (u_long i = 0; i < (vec_orig.size() / 2); i++) {
+		if (left[i] > right[i])
+			std::swap(left[i], right[i]);
+	}
+	T old_right = right;
+	sort(right);
+	T old_left = left;
+	for (u_long i = 0; i < right.size(); i++) {
+		typename T::iterator it = std::find(old_right.begin(), old_right.end(), right[i]);
+		if (it != old_right.end()) {
+			int index = std::distance(old_right.begin(), it);
+			left[i] = old_left[index];
+		}
+	}
+	T result = right;
+	if (leftover != 0) {
+		left.push_back(leftover);
+	}
+	result.insert(result.begin(), left[0]);
+	if (left.begin() + 1 != left.end())
+		result.insert(result.begin() + binary_search(result, left[1]), left[1]);
+	std::vector<int> const & jacobsthal_numbers = getJacobsthalNumbers();
+	int index;
+	for (int i = 2; i <= binary_search(jacobsthal_numbers, left.size() - 1); i++) {
+		if ((static_cast<int>(left.size()) - 1) < jacobsthal_numbers[i]) {
+			index = static_cast<int>(left.size()) - 1;
+		} else {
+			index = jacobsthal_numbers[i];
+		}
+		while (index != jacobsthal_numbers[i - 1]) {
+			result.insert(result.begin() + binary_search(result, left[index]), left[index]);
+			index--;
+		}
+	}
+	vec_orig = result;
+}
+
 
 #endif
